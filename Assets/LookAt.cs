1899692a5915a17cf2915df1bf991a829cab899c
Ryo Okada <okada.ryo@hpe.com>
2018-02-04 20:18:13 +0900

    first commit

---
+++ Assets/LookAt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour {
+
+	[SerializeField]
+	[Tooltip("ターゲットとなるオブジェクト")]
+	private Transform _target;
+
+	private HeadLookController _headLookController;
+
+	// Use this for initialization
+	private void Start () {
+		_headLookController = GetComponent<HeadLookController> ();
+	}
+	
+	// Update is called once per frame
+	private void LateUpdate () {
+		_headLookController.target = _target.position;
+	}
+}
